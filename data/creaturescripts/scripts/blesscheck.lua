ee925e09bcca47dbbdfc6b79d3ce35b51ae6c440
dominique120@live.com <dominique120@live.com>
2013-12-20 22:57:14 -0500

    Fixed issue #10

--- data/creaturescripts/scripts/blesscheck.lua
+++ data/creaturescripts/scripts/blesscheck.lua
@@ -1,9 +1,9 @@
 --Script by Amiroslo
 function onLogin(cid)
 if(getPlayerBlessing(cid, 1)) then
-doPlayerSendTextMessage(cid,13,'You are blessed!')
+doPlayerSendTextMessage(cid,16,'You are blessed!')
 else
-doPlayerSendTextMessage(cid,5,'You are not blessed')
+doPlayerSendTextMessage(cid,4,'You are not blessed')
 end
 return true
 end
