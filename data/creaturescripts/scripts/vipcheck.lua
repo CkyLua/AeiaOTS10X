7bb45432aaeb29bda44032a7773b5e48b9ab83f6
dominique120@live.com <dominique120@live.com>
2013-12-22 21:53:03 -0500

    Adding some missing creatures and changin the messate type for the VIP notifier.

--- data/creaturescripts/scripts/vipcheck.lua
+++ data/creaturescripts/scripts/vipcheck.lua
@@ -1,7 +1,7 @@
 --- Script by Kekox
 function onLogin(cid)
 if getPlayerVipDays(cid) >= 1 then
-doPlayerSendTextMessage(cid, 19, "You have ".. getPlayerVipDays(cid) .." vip days left.")
+doPlayerSendTextMessage(cid, 4, "You have ".. getPlayerVipDays(cid) .." vip days left.")
 end
 return true
 end
