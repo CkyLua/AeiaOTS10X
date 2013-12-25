094400e7f0b34a9d56b09119b8e923aeb47bd6b6
dominique120@live.com <dominique120@live.com>
2013-12-24 21:03:29 -0500

    Adding many functions by Limos from OTLand.

--- data/global.lua
+++ data/global.lua
@@ -642,6 +642,174 @@ ITEM_WILDGROWTH_PERSISTENT = 2721
 ITEM_WILDGROWTH_SAFE = 11099
 
 
+
+
+--[[//////////////////////////////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+/////////////////extra functions//////////////////
+////////////////////by Limos//////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+]]
+
+function createClass(parent)
+   local newClass = {}
+   function newClass:new(instance)
+     local instance = instance or {}
+     setmetatable(instance, {__index = newClass})
+     return instance
+   end
+
+   if(parent ~= nil) then
+     setmetatable(newClass, {__index = parent})
+   end
+
+   function newClass:getSelf()
+     return newClass
+   end
+
+   function newClass:getParent()
+     return baseClass
+   end
+
+   function newClass:isa(class)
+     local tmp = newClass
+     while(tmp ~= nil) do
+       if(tmp == class) then
+         return true
+       end
+
+       tmp = tmp:getParent()
+     end
+
+     return false
+   end
+
+   function newClass:setAttributes(attributes)
+     for k, v in pairs(attributes) do
+       newClass[k] = v
+     end
+   end
+
+   return newClass
+end
+
+Result = createClass(nil)
+Result:setAttributes({
+   id = -1,
+   query = ""
+})
+
+function Result:getID()
+   return self.id
+end
+
+function Result:setID(_id)
+   self.id = _id
+end
+
+function Result:getQuery()
+   return self.query
+end
+
+function Result:setQuery(_query)
+   self.query = _query
+end
+
+function Result:create(_query)
+   self:setQuery(_query)
+   local _id = db.storeQuery(self:getQuery())
+   if(_id) then
+     self:setID(_id)
+   end
+
+   return self:getID()
+end
+
+function Result:getRows(free)
+   local free = free or false
+   if(self:getID() == -1) then
+     error("[Result:getRows] Result not set!")
+   end
+
+   local c = 0
+   repeat
+     c = c + 1
+   until not self:next()
+
+   local _query = self:getQuery()
+   self:free()
+   if(not free) then
+     self:create(_query)
+   end
+
+   return c
+end
+
+function Result:getDataInt(s)
+   if(self:getID() == -1) then
+     error("[Result:getDataInt] Result not set!")
+   end
+
+   return result.getDataInt(self:getID(), s)
+end
+
+function Result:getDataLong(s)
+   if(self:getID() == -1) then
+     error("[Result:getDataLong] Result not set!")
+   end
+
+   return result.getDataLong(self:getID(), s)
+end
+
+function Result:getDataString(s)
+   if(self:getID() == -1) then
+     error("[Result:getDataString] Result not set!")
+   end
+
+   return result.getDataString(self:getID(), s)
+end
+
+function Result:getDataStream(s)
+   if(self:getID() == -1) then
+     error("[Result:getDataStream] Result not set!")
+   end
+
+   return result.getDataStream(self:getID(), s)
+end
+
+function Result:next()
+   if(self:getID() == -1) then
+     error("[Result:next] Result not set!")
+   end
+
+   return result.next(self:getID())
+end
+
+function Result:free()
+   if(self:getID() == -1) then
+     error("[Result:free] Result not set!")
+   end
+
+   self:setQuery("")
+   local ret = result.free(self:getID())
+   self:setID(-1)
+   return ret
+end
+
+Result.numRows = Result.getRows
+function db.getResult(query)
+   if(type(query) ~= 'string') then
+     return nil
+   end
+
+   local ret = Result:new()
+   ret:create(query)
+   return ret
+end
+--End of extra functions by limos
+
 --custom functions
 
 --marriage stuff
@@ -701,48 +869,6 @@ function isOnline(player)
 	else
 		return 0
 	end
-end
-
-function createClass(parent)
-   local newClass = {}
-   function newClass:new(instance)
-     local instance = instance or {}
-     setmetatable(instance, {__index = newClass})
-     return instance
-   end
-
-   if(parent ~= nil) then
-     setmetatable(newClass, {__index = parent})
-   end
-
-   function newClass:getSelf()
-     return newClass
-   end
-
-   function newClass:getParent()
-     return baseClass
-   end
-
-   function newClass:isa(class)
-     local tmp = newClass
-     while(tmp ~= nil) do
-       if(tmp == class) then
-         return true
-       end
-
-       tmp = tmp:getParent()
-     end
-
-     return false
-   end
-
-   function newClass:setAttributes(attributes)
-     for k, v in pairs(attributes) do
-       newClass[k] = v
-     end
-   end
-
-   return newClass
 end
 
 function Result:create(_query)
@@ -826,6 +952,15 @@ function doRemoveVipDays(cid, days)
 	db.executeQuery("UPDATE `accounts` SET `vipdays` = `vipdays` - " .. days .. " WHERE `id` = " .. getAccountNumberByPlayerName(name) .. ";")
 end
 
+function getPlayerNameByGUID(guid)
+     local name = 0
+     local resultId = db.getResult("SELECT `name` FROM `players` WHERE `id` = " .. guid)
+     if(resultId:getID() ~= -1) then
+           name = resultId.getDataString(resultId, "name")
+           resultId:free()
+     end
+     return name
+end
 
 
 --End custom funcs
