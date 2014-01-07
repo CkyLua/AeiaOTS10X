04ca3387aac66b1c8b3eed75ec6b0b22047d68c2
dominique120@live.com <dominique120@live.com>
2014-01-07 17:43:34 -0500

    Update an complete compile script.

--- ncompile.sh
+++ ncompile.sh
@@ -1,12 +1,18 @@
+﻿#!/bin/bash
 ###
 ### Functions to simplify stuff :)
 ###
+
 debianDeps() {
-apt-get install git cmake build-essential liblua5.2-dev libgmp3-dev libmysqlclient-dev libboost-system-dev
-}	
+	apt-get install git cmake build-essential liblua5.2-dev \
+		libgmp3-dev libmysqlclient-dev libboost-system-dev
+}
+
 fedoraDeps() {
-yum install git cmake gcc-c++ boost-devel gmp-devel community-mysql-devel lua-devel
-}	
+	yum install git cmake gcc-c++ boost-devel \
+		gmp-devel community-mysql-devel lua-devel
+}
+
 bsdDeps() {
 	cd /usr/ports/shells/bash && make install clean BATCH=yes
 	cd /usr/ports/devel/git && make install clean BATCH=yes
@@ -17,70 +23,97 @@ bsdDeps() {
 	cd /usr/ports/math/gmp && make install clean BATCH=yes
 	cd /usr/ports/databases/mysql-connector-c && make install clean BATCH=yes
 }
+
 libInstall() {
 	echo "Libraries and Build Tools... Installed"
 }
+
 bsdBuild() {
 	echo "Building on FreeBSD"
 	mkdir build && cd build
 	CXX=g++47 cmake ..
 	make
 }	
+
 genBuild() {
 	echo "Building..."
 	mkdir build && cd build
 	cmake ..
 	make
 }
+
 clean() {
 	mv *.cpp *.h src/ && mv *.o objs/
 	echo "There might be a few leftover files."
 }
+
 ###
 ### Script starts here
 ###
+
+#check if root
 if [[ $EUID -ne 0 ]]; then
 	echo "You must be root to use this script, press enter to exit."
 	read end
 	exit 1
-
-		echo "Chose your Operating System. {Supported OS: Debian, Ubuntu, Fedora, CentOS, FreeBSD} "
-			read ans1 
+fi
+#OS dependencies and other stuff
+echo "Chose your Operating System. {Supported OS: Debian, Ubuntu, Fedora, CentOS, FreeBSD} "
+read ans1 
 			
-				if [[ $ans1 = "Fedora" ]] || [[ $nas1 = "CentOS" ]]; then
-					echo -n "Should the script install dependencies? y/n"
-						if [[ $ans1_1 = "y" ]]; then
-							fedoraDeps
-						elif [[ $ans1_1 = "n" ]]; then
-							break
-						else
-							echo "Answer '\y\' or \'n\' "
-						fi
-				elif [[ $ans1 = "Debian" ]] || [[ $ans1 = "Ubuntu" ]]; then
-					echo -n "Should the script install dependencies? y/n"
-						if [[ $ans1_1 = "y" ]]; then
-							debianDeps
-						elif [[ $ans1_1 = "n" ]]; then
-							break
-						else
-							echo "Answer '\y\' or \'n\' "
-						fi
-				elif [[ $ans1 = "FreeBSD" ]]; then
-					echo -n "Should the script install dependencies? y/n"
-						if [[ $ans1_1 = "y" ]; then
-							bsdDeps
-						elif [[ $ans1_1 = "n" ]]; then
-							break
-						else
-							echo "Answer '\y\' or \'n\' "
-						fi
-						
-				else
-					echo "Pick a valid OS"
-					
-				fi
-				## temporary fi
-				fi
-				
+if [[ $ans1 = "Fedora" ]] || [[ $nas1 = "CentOS" ]]; then
+	echo -n "Should the script install dependencies? y or n"
+	read ans1_1
+	if [[ $ans1_1 = "y" ]]; then
+		fedoraDeps
+	elif [[ $ans1_1 = "n" ]]; then
+		break
+	else
+		echo "Answer 'y' or 'n' "
+	fi
+elif [[ $ans1 = "Debian" ]] || [[ $ans1 = "Ubuntu" ]]; then
+	echo -n "Should the script install dependencies? y or n"
+	read ans1_1
+	if [[ $ans1_1 = "y" ]]; then
+		debianDeps
+	elif [[ $ans1_1 = "n" ]]; then
+		break
+	else
+		echo "Answer 'y' or 'n' "
+	fi
+elif [[ $ans1 = "FreeBSD" ]]; then
+	echo -n "Should the script install dependencies? y or n"
+	read ans1_1
+		if [[ $ans1_1 = "y" ]]; then
+			bsdDeps
+		elif [[ $ans1_1 = "n" ]]; then
+			break
+		else
+			echo "Answer 'y' or 'n' "
+		fi				
+		else
+			echo "Pick a valid OS"		
+		fi
+		
+#Compiling here
+
+echo -n "Are we on FreeBSD? y or n"
+read ans1_2
+	if [[ $ans1_2 = "y" ]]; then
+		bsdbuild
+	elif [[ $ans1_2 = "n" ]]; then
+		genBuild
+	else
+		echo "Answer y or n"
+	fi
 	
-	
+echo "Should the folder be cleaned? y or n"
+	read ans1_3
+	if [[ $ans1_3 = "y" ]]; then
+		clean
+	elif [[ $ans1_3 = "n" ]]; then
+		echo "Exiting..."
+		exit 1
+	else
+		echo "Answer y or n"
+	fi
