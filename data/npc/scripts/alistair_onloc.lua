cc7e138c062b58d77ec79fea37181b109eed89df
dominique120@live.com <dominique120@live.com>
2014-01-26 22:11:59 -0500

    Mappin, fixing bugs and added an NPC.

--- data/npc/scripts/alistair_onloc.lua
+++ data/npc/scripts/alistair_onloc.lua
@@ -1,7 +1,7 @@
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
-local random_texts = {'Just feel that wind.', 'What a sunny day to slain some sea creatures!', 'I hope that lizards didn\'t see us.'}
+local random_texts = {'Look at \'em sea serpents.', 'Watch out for the cult guys.', 'What is so interesting about this place anyway?', 'Someone spotted three headed dragons out there.'}
 local random_texts_chance = 10 -- percent
 local random_texts_interval = 5 -- seconds
 local talkState = {}
@@ -24,11 +24,9 @@ function onPlayerCloseChannel(cid)			npcHandler:onPlayerCloseChannel(cid)		end
 		function greetCallback(cid)
 	local talkUser = NPCHANDLER_CONVBEHAVIOR == CONVERSATION_DEFAULT and 0 or cid
 	local msg = {
-		"The water is cold a bit, but should be good for you.", 
-		"So, " .. getPlayerName(cid) .. " are you ready to dive?", 
-		"I hope that lizards didn\'t see us.", 
-		"Welcome.", 
-		"I hope at least you have better day than me."
+		"Speak", 
+		"Ready to go?",
+		"Do you want to go {back}?"
 	}
 	npcHandler:setMessage(MESSAGE_GREET, msg[math.random(1, #msg)])
 	talkState[talkUser] = 0
