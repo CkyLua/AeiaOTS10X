e77cd1e00129c2d32c113bc280cd33fef34057d2
dominique120@live.com <dominique120@live.com>
2014-01-30 23:43:38 -0500

    Working on the site.

--- www/serverinfo.php
+++ www/serverinfo.php
@@ -1,7 +1,11 @@
 <?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
 
-<h1>Server Information</h1>
-<p>Staged experience.</p>
+<h1>Server Tech Specs</h1>
+<p>This server and its components(webserver/tibiaserver) are hosted in OVH's RBX datacentre in Roubaix, France.</p>
+<p>The server has 16GBs or ram and runs on an Intel CPU with 4 cores protected by the datacentre's 160GBps DDoS mitigation system, on a T1 </p>
+
+<br><br><br><br>
+<img border="0" src="/logos/logos.png" >
 
 
 
