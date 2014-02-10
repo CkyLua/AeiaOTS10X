073b6e1264dc9a9a3640741d0ecc8c4dc156833b
dominique120@live.com <dominique120@live.com>
2014-02-09 23:35:28 -0500

    Update a script.

--- data/talkactions/scripts/jailing.lua
+++ data/talkactions/scripts/jailing.lua
@@ -1,132 +1,4 @@
 --[[
-OTLAND.NET / CREDITS
-  CREATED BY:
-    master-m
-  EDITED BY:
-    Lithium: Shorten.
-    Velik: Missing parts. Tested code.
-    Colandus: Missing end. Works for both TFS and Evolutions. Added more "checks". Shorten alot.
-]]
---[[
--- Start the function --
-function onSay ( cid, words, param )
-
--- Edit these positions bewlow --
-
-	-- The permission you need to jail someone --
-	grouprequired = 3
-	
-	-- StorageValue that the player gets --
-	jailedstoragevalue = 1338
-
-	-- Set the position of the jail: --
-	jailpos = {x = 1140, y = 939, z = 7}
-	
-	-- Set the position once unjailed: --
-	unjailpos = { x = 1000, y = 998, z = 7 } 
-
--- Stop edting unless you know what you are doing --	
-
-	-- Executes the code below if it is the word is /jail --
-	if words == '/jail' then	
-	
-		-- Checks if the player is allowed to jail someone, said a playername to jail and looks if the player isn't in jail already --
-		if getPlayerGroupId ( cid ) >= grouprequired and param ~= "" and getPlayerStorageValue ( getPlayerByName ( param ), jailedstoragevalue ) == -1 then 
-		
-			-- The actual code --
-			
-			-- Gets the name of the one who jails someone --
-			jailer = getPlayerName ( cid ) 
-			
-			-- Teleporting the person to jail --
-			doTeleportThing ( getPlayerByName ( param ), jailpos, 0 )
-			
-			-- Sends a message to the jailed person --
-			doPlayerSendTextMessage ( getPlayerByName ( param ), 25, 'You have been jalied by '..jailer..'' ) 
-			
-			-- Sends a message to the one who jails someone --
-			doPlayerSendTextMessage ( cid, 21, "You just jailed "..param.."." )
-			
-			-- Gives the player a storage value to remember that he is jailed --
-			setPlayerStorageValue ( getPlayerByName ( param ), jailedstoragevalue, 1 )
-			
-		-- end of the actual code --
-			
-			
-		-- What to do if the player don't have the permission --
-		elseif getPlayerGroupId ( cid ) < grouprequired then
-		
-			-- Sends the player a message that he doesn't have permission to jail someone --
-			doPlayerSendTextMessage ( cid, 21, "You are not permitted to jail anyone." )
-			
-		-- What to do if the player is already jailed --
-		elseif getPlayerStorageValue ( getPlayerByName ( param ), jailedstoragevalue ) ~= -1 then
-		
-			-- Sends the player a message if the player is already jailed --
-			doPlayerSendTextMessage ( cid, 21, "This Player is already jailed." )
-		
-		-- What to do if the player didn't fill in a name --		
-		else
-				-- Sends the player a message that he forgot to fill in a playername --
-				doPlayerSendTextMessage ( cid, 21, "You need to fill in a playername!" )
-		
-		-- Ends the checks --
-		end
-		
-	-- Executes the code below if it is the word is /unjail --
-	elseif words == '/unjail' then
-	
-		-- Checks if the player is allowed to unjail someone, said a playername to unjail and looks if the player isn't in unjailed already --
-		if getPlayerGroupId ( cid ) >= grouprequired and param ~= "" and getPlayerStorageValue ( getPlayerByName ( param ), jailedstoragevalue ) == 1 then 
-		
-			-- The actual code --
-			
-			-- Gets the name of the one who unjails someone --
-			unjailer = getPlayerName ( cid )
-			
-			-- Teleporting the person outside the jail --
-			doTeleportThing ( getPlayerByName ( param ), unjailpos, 0 )
-			
-			-- Sends a message to the unjailed person --
-			doPlayerSendTextMessage ( getPlayerByName ( param ), 25, 'You have been unjailed by '..unjailer..'' )
-			
-			-- Sends a message to the one who unjailed someone --
-			doPlayerSendTextMessage ( cid, 21,"You just unjailed "..param.."." )
-			
-			-- Removes the players  storage value so we know that he isn't jailed anymore. --
-			setPlayerStorageValue ( getPlayerByName ( param ), jailedstoragevalue, -1 )
-			
-			-- end of the actual code --
-			
-		
-		-- What to do if the player don't have the permission --		
-		elseif getPlayerGroupId ( cid ) < 4 then
-		
-			-- Sends the player a message that he doesn't have permission to unjail someone --
-			doPlayerSendTextMessage ( cid, 21, "You cannot unjail someone!" )
-		
-		-- What to do if the player is already unjailed --
-		elseif getPlayerStorageValue ( getPlayerByName ( param ), jailedstoragevalue ) ~= 1 then
-		
-			-- Sends the player a message if the player is already unjailed --
-			doPlayerSendTextMessage ( cid, 21, "This Player is already unjailed." )
-			
-		-- What to do if the player didn't fill in a name --	
-		else
-		
-			-- Sends the player a message that he forgot to fill in a playername --
-			doPlayerSendTextMessage ( cid, 21, "You need to fill in a playername!" )
-			
-		-- Ends the checks --	
-		end
-		
-	-- Ends the ( un ) jailing part --	
-	end		
-	
--- Ends the function --	
-end
-]]
-
 
 
 local config = {
@@ -175,3 +47,105 @@ function onSay (cid, words, param)
         setPlayerStorageValue(target, config.storage, -1)
     end      
 end
+]]
+grouprequired = 2
+jailedstoragevalue_time = 1338
+jailedstoragevalue_bool = 1339
+local jailpos = { 
+    [1] = {x = 1140, y = 939, z = 7},
+    [2] = {x = 1140, y = 939, z = 7},
+    [3] = {x = 1140, y = 939, z = 7},
+    [4] = {x = 1140, y = 939, z = 7},
+    [5] = {x = 1140, y = 939, z = 7},
+    [6] = {x = 1140, y = 939, z = 7},
+    [7] = {x = 1140, y = 939, z = 7},
+    [8] = {x = 1140, y = 939, z = 7}
+}
+local unjailpos = { x = 1000, y = 1000, z =7 }
+jail_list = {}
+jail_list_work = 0
+
+function checkJailList(param)
+    addEvent(checkJailList, 1000, {})
+    for targetID,player in ipairs(jail_list) do
+        if isPlayer(player) == TRUE then
+            if getPlayerStorageValue(player, jailedstoragevalue_time) < os.time() then
+                doTeleportThing(player, unjailpos, TRUE)
+                setPlayerStorageValue(player, jailedstoragevalue_time, 0)
+                setPlayerStorageValue(player, jailedstoragevalue_bool, 0)
+                table.remove(jail_list,targetID)
+                doPlayerSendTextMessage(player, MESSAGE_STATUS_CONSOLE_ORANGE, 'You got out of jail, try not to do evil things next time to avoid being arrested again. Take care friend.')
+            end
+        else
+            table.remove(jail_list,targetID)
+        end
+    end
+end
+
+function onSay(cid, words, param)
+    if not Player(cid):getGroup():getAccess() then
+        return false
+    end
+    if(param == '') then
+        doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Command param required.")
+        return false
+    end
+    local t = param:split(", ")
+    if jail_list_work == 0 then
+        jail_list_work = addEvent(checkJailList, 1000, {})
+    end
+    local jail_time = -1
+    for word in string.gmatch(tostring(t[1]), "(%w+)") do
+        if tostring(tonumber(word)) == word then
+            jail_time = tonumber(word)
+        end
+    end
+    local isplayer = getPlayerByName(t[1])
+    if isPlayer(isplayer) ~= TRUE then
+        isplayer = getPlayerByName(string.sub(t[1], string.len("jail_time")+1))
+        if isPlayer(isplayer) ~= TRUE then
+            isplayer = getPlayerByName(string.sub(t[1], string.len("jail_time")+2))
+            if isPlayer(isplayer) ~= TRUE then
+                isplayer = getPlayerByName(string.sub(t[1], string.len("jail_time")+3))
+            end
+        end
+    end
+    local default_jail = 30
+    if(t[2]) then
+        default_jail = t[2]
+    end
+    if jail_time ~= -1 then
+        jail_time = jail_time * 60
+    else
+        jail_time = default_jail
+    end
+   
+    if (words == '/jail') then
+        if isPlayer(isplayer) == TRUE then
+            doTeleportThing(isplayer, jailpos[math.random(#jailpos)], TRUE)
+            setPlayerStorageValue(isplayer, jailedstoragevalue_time, os.time()+jail_time)
+            setPlayerStorageValue(isplayer, jailedstoragevalue_bool, 1)
+            table.insert(jail_list,isplayer)
+            doPlayerSendTextMessage (cid, MESSAGE_STATUS_CONSOLE_ORANGE, 'You arrested the player '.. getCreatureName(isplayer) ..' until ' .. os.date("%H:%M:%S", getPlayerStorageValue(isplayer, jailedstoragevalue_time)) .. ' (now is: ' .. os.date("%H:%M:%S", os.time()) .. ').')
+            doPlayerSendTextMessage (isplayer, MESSAGE_STATUS_CONSOLE_ORANGE, 'You were arrested for '.. getCreatureName(cid) ..' until ' .. os.date("%H:%M:%S", getPlayerStorageValue(isplayer, jailedstoragevalue_time)) .. ' (now is: ' .. os.date("%H:%M:%S", os.time()) .. ').')
+        else
+            doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "This player does not exist or is offline.")
+        end
+    elseif (words == '/unjail') then
+        if isPlayer(isplayer) == TRUE then
+            if getPlayerStorageValue(isplayer, jailedstoragevalue_bool) == 1 then
+                doTeleportThing(isplayer, unjailpos, TRUE)
+                setPlayerStorageValue(isplayer, jailedstoragevalue_time, 0)
+                setPlayerStorageValue(isplayer, jailedstoragevalue_bool, 0)
+                table.remove(jail_list,targetID)
+                doPlayerSendTextMessage(isplayer, MESSAGE_STATUS_CONSOLE_ORANGE, 'The player '.. getCreatureName(cid) ..' brought you out of jail. See you soon!')
+                doPlayerSendTextMessage (cid, MESSAGE_STATUS_CONSOLE_ORANGE, 'You removed out the player '.. getCreatureName(isplayer) ..'of the jail.')
+            else
+                doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "This player is not stuck.")
+            end
+        else
+            doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "This player does not exist or is offline.")
+        end
+    end
+    return false
+end 
